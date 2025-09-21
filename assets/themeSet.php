6b6fae5426044f89c08d0dd101c7fa71f9042a59
shubham kumar <suraj2002mail@gmail.com>
2025-09-21 17:22:29 +0530

    updated code from xss attack

--- assets/themeSet.php
+++ assets/themeSet.php
@@ -3,23 +3,21 @@ include("config.php");
 session_start();
 
 if(isset($_SESSION['uid'])){
-
     if(isset($_POST['theme'])){
         $theme = $_POST['theme'];
         $uid = $_SESSION['uid'];
-        echo $theme;
 
-        // Using prepared statement
+        $allowed_themes = ['light', 'dark'];
+        if (!in_array($theme, $allowed_themes)) {
+            $theme = 'light';
+        }
+
+        echo htmlspecialchars($theme, ENT_QUOTES, 'UTF-8');
+
         $query = "UPDATE `users` SET `theme` = ? WHERE `users`.`id` = ?";
         $stmt = mysqli_prepare($conn, $query);
-
-        // Bind parameters
         mysqli_stmt_bind_param($stmt, "ss", $theme, $uid);
-
-        // Execute the prepared statement
         mysqli_stmt_execute($stmt);
-
-        // Close the prepared statement
         mysqli_stmt_close($stmt);
     }
 }
